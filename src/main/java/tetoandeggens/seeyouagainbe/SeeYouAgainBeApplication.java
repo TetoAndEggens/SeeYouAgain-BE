edd0b1fe16c11090db7a7b85f439876ccfcb594c
bronze_yun_4521 <ehddbs452100@naver.com>
2025-09-29 17:30:56 +0900

    feat: init project

---
+++ src/main/java/tetoandeggens/seeyouagainbe/SeeYouAgainBeApplication.java
@@ -0,0 +1,13 @@
+package tetoandeggens.seeyouagainbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeYouAgainBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeYouAgainBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tetoandeggens/seeyouagainbe/SeeYouAgainBeApplicationTests.java
@@ -0,0 +1,13 @@
+package tetoandeggens.seeyouagainbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeYouAgainBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
