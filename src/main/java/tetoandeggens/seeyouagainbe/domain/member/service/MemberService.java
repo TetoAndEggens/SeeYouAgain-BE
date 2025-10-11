bf84044313e3404b76ce34908ce989064148fe90
JWMin556~ <min981626@gmail.com>
2025-10-11 12:53:55 +0900

    refactor: #1 implement soft delete and prepare for social login from Member package

--- src/main/java/tetoandeggens/seeyouagainbe/domain/member/service/MemberService.java
+++ src/main/java/tetoandeggens/seeyouagainbe/domain/member/service/MemberService.java
@@ -6,7 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import tetoandeggens.seeyouagainbe.domain.member.entity.Member;
 import tetoandeggens.seeyouagainbe.domain.member.repository.MemberRepository;
 import tetoandeggens.seeyouagainbe.global.exception.CustomException;
-import tetoandeggens.seeyouagainbe.global.exception.MemberErrorCode;
+import tetoandeggens.seeyouagainbe.global.exception.errorcode.MemberErrorCode;
 
 @Service
 @Transactional(readOnly = true)
@@ -15,31 +15,29 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public Member getMemberById(Long memberId) {
-        return memberRepository.findById(memberId)
+    public Member findByLoginId(String loginId) {
+        return memberRepository.findByLoginIdAndIsDeletedFalse(loginId)
                 .orElseThrow(() -> new CustomException(MemberErrorCode.MEMBER_NOT_FOUND));
     }
 
-    public Member getMemberByUuid(String uuid) {
-        return memberRepository.findByUuid(uuid)
+    public Member findByPhoneNumber(String phoneNumber) {
+        return memberRepository.findByPhoneNumberAndIsDeletedFalse(phoneNumber)
+                .orElseThrow(() -> new CustomException(MemberErrorCode.MEMBER_NOT_FOUND));
+    }
+
+    public Member findByUuid(String uuid) {
+        return memberRepository.findByUuidAndIsDeletedFalse(uuid)
+                .orElseThrow(() -> new CustomException(MemberErrorCode.MEMBER_NOT_FOUND));
+    }
+
+    public Member findById(Long memberId) {
+        return memberRepository.findByIdAndIsDeletedFalse(memberId)
                 .orElseThrow(() -> new CustomException(MemberErrorCode.MEMBER_NOT_FOUND));
     }
 
     @Transactional
-    public void updateNickName(Long memberId, String newNickName) {
-        Member member = getMemberById(memberId);
-        member.updateNickName(newNickName);
-    }
-
-    @Transactional
-    public void updateProfile(Long memberId, String profileUrl) {
-        Member member = getMemberById(memberId);
-        member.updateProfile(profileUrl);
-    }
-
-    @Transactional
-    public void togglePushNotification(Long memberId) {
-        Member member = getMemberById(memberId);
-        member.togglePushNotification();
+    public void softDeleteMember(Long memberId) {
+        Member member = findById(memberId);
+        member.updateDeleteStatus();
     }
 }
