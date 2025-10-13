0e373d2f55ea44f8a78bd101aeef705ddecb3348
JWMin556~ <min981626@gmail.com>
2025-10-14 02:09:37 +0900

    refactor: #1 remove unnecessary comments

---
+++ src/main/java/tetoandeggens/seeyouagainbe/auth/filter/FilterExceptionHandler.java
@@ -0,0 +1,40 @@
+package tetoandeggens.seeyouagainbe.auth.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class FilterExceptionHandler extends OncePerRequestFilter {
+
+    private final HandlerExceptionResolver resolver;
+
+    public FilterExceptionHandler(
+            @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver
+    ) {
+        this.resolver = resolver;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception ex) {
+            log.error("Spring Security 필터 체인에서 예외 발생: {}", ex.getMessage(), ex);
+            resolver.resolveException(request, response, null, ex);
+        }
+    }
+}
