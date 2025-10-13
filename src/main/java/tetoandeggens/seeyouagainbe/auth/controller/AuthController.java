d4d54644f38ba5361a62089e12f9d4758ed4ded9
JWMin556~ <min981626@gmail.com>
2025-10-14 00:28:45 +0900

    refactor: #1 update AuthErrorCode with relating files

--- src/main/java/tetoandeggens/seeyouagainbe/auth/controller/AuthController.java
+++ src/main/java/tetoandeggens/seeyouagainbe/auth/controller/AuthController.java
@@ -7,7 +7,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import tetoandeggens.seeyouagainbe.auth.dto.request.PhoneVerificationRequest;
 import tetoandeggens.seeyouagainbe.auth.dto.request.RegisterRequest;
+import tetoandeggens.seeyouagainbe.auth.dto.response.PhoneVerificationResultResponse;
 import tetoandeggens.seeyouagainbe.auth.dto.response.ReissueTokenResponse;
 import tetoandeggens.seeyouagainbe.auth.service.AuthService;
 import tetoandeggens.seeyouagainbe.global.response.ApiResponse;
@@ -19,6 +21,47 @@ import tetoandeggens.seeyouagainbe.global.response.ApiResponse;
 public class AuthController {
 
     private final AuthService authService;
+
+    @Operation(
+            summary = "loginId 중복 체크",
+            description = "로그인 아이디 중복 확인"
+    )
+    @GetMapping("/check/loginId")
+    public ApiResponse<Void> checkLoginId(@RequestParam String loginId) {
+        authService.checkLoginIdAvailable(loginId);
+        return ApiResponse.noContent();
+    }
+
+    @Operation(
+            summary = "휴대폰 번호 중복 체크",
+            description = "휴대폰 번호 중복 확인"
+    )
+    @PostMapping("/check/phone")
+    public ApiResponse<Void> checkPhoneNumber(@Valid @RequestBody PhoneVerificationRequest request) {
+        authService.checkPhoneNumberDuplicate(request.phone());
+        return ApiResponse.noContent();
+    }
+
+    @Operation(
+            summary = "휴대폰 인증 코드 요청",
+            description = "휴대폰 인증을 위한 코드 생성. 응답으로 받은 code를 서버 이메일 주소로 SMS 전송 필요"
+    )
+    @PostMapping("/phone/send-code")
+    public ApiResponse<PhoneVerificationResultResponse> sendPhoneVerificationCode(
+            @Valid @RequestBody PhoneVerificationRequest request) {
+        PhoneVerificationResultResponse response = authService.sendPhoneVerificationCode(request.phone());
+        return ApiResponse.ok(response);
+    }
+
+    @Operation(
+            summary = "휴대폰 인증 코드 검증",
+            description = "사용자가 SMS로 보낸 인증 코드를 서버가 이메일로 확인하여 검증"
+    )
+    @PostMapping("/phone/verify-code")
+    public ApiResponse<Void> verifyPhoneCode(@Valid @RequestBody PhoneVerificationRequest request) {
+        authService.verifyPhoneCode(request.phone());
+        return ApiResponse.noContent();
+    }
 
     @Operation(
             summary = "회원가입",
@@ -41,15 +84,5 @@ public class AuthController {
     ) {
         ReissueTokenResponse reissueResponse = authService.reissueToken(request, response);
         return ApiResponse.ok(reissueResponse);
-    }
-
-    @Operation(
-            summary = "loginId 중복 체크",
-            description = "로그인 아이디 중복 확인"
-    )
-    @GetMapping("/check/loginId")
-    public ApiResponse<Void> checkLoginId(@RequestParam String loginId) {
-        authService.checkLoginIdAvailable(loginId);
-        return ApiResponse.noContent();
     }
 }
--- src/main/java/tetoandeggens/seeyouagainbe/auth/service/AuthService.java
+++ src/main/java/tetoandeggens/seeyouagainbe/auth/service/AuthService.java
@@ -3,18 +3,28 @@ package tetoandeggens.seeyouagainbe.auth.service;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tetoandeggens.seeyouagainbe.auth.dto.request.RegisterRequest;
+import tetoandeggens.seeyouagainbe.auth.dto.response.PhoneVerificationResultResponse;
 import tetoandeggens.seeyouagainbe.auth.dto.response.ReissueTokenResponse;
 import tetoandeggens.seeyouagainbe.auth.jwt.TokenProvider;
+import tetoandeggens.seeyouagainbe.auth.util.GeneratorRandomUtil;
 import tetoandeggens.seeyouagainbe.domain.member.entity.Member;
 import tetoandeggens.seeyouagainbe.domain.member.entity.SocialType;
 import tetoandeggens.seeyouagainbe.domain.member.repository.MemberRepository;
 import tetoandeggens.seeyouagainbe.global.exception.errorcode.AuthErrorCode;
 import tetoandeggens.seeyouagainbe.global.exception.CustomException;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static tetoandeggens.seeyouagainbe.global.constants.EmailVerificationConstant.*;
+
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -23,28 +33,29 @@ public class AuthService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
+    private final RedisTemplate<String, String> redisTemplate;
+    private final EmailService emailService;
 
     @Transactional
     public void register(RegisterRequest registerRequest) {
-        if (memberRepository.existsByLoginIdAndIsDeletedFalse(registerRequest.loginId())) {
-            throw new CustomException(AuthErrorCode.DUPLICATED_LOGIN_ID);
+        String isVerified = redisTemplate.opsForValue().get(registerRequest.phoneNumber());
+
+        if (isVerified == null || !isVerified.equals(VERIFIED)) {
+            throw new CustomException(AuthErrorCode.PHONE_NOT_VERIFIED);
         }
 
-        if (memberRepository.existsByPhoneNumberAndIsDeletedFalse(registerRequest.phoneNumber())) {
-            throw new CustomException(AuthErrorCode.DUPLICATED_PHONE_NUMBER);
-        }
-
-        String encodedPassword = passwordEncoder.encode(registerRequest.password());
+        checkLoginIdAvailable(registerRequest.loginId());
 
         Member member = Member.builder()
                 .loginId(registerRequest.loginId())
-                .password(encodedPassword)
+                .password(passwordEncoder.encode(registerRequest.password()))
                 .nickName(registerRequest.nickName())
                 .phoneNumber(registerRequest.phoneNumber())
-                .type(SocialType.GENERAL)
                 .build();
 
         memberRepository.save(member);
+
+        redisTemplate.delete(registerRequest.phoneNumber());
     }
 
     public ReissueTokenResponse reissueToken(HttpServletRequest request, HttpServletResponse response) {
@@ -65,5 +76,47 @@ public class AuthService {
         if (memberRepository.existsByLoginIdAndIsDeletedFalse(loginId)) {
             throw new CustomException(AuthErrorCode.DUPLICATED_LOGIN_ID);
         }
+    }
+
+    public void checkPhoneNumberDuplicate(String phoneNumber) {
+        if (memberRepository.existsByPhoneNumberAndIsDeletedFalse(phoneNumber)) {
+            throw new CustomException(AuthErrorCode.PHONE_NUMBER_DUPLICATED);
+        }
+    }
+
+    @Transactional
+    public PhoneVerificationResultResponse sendPhoneVerificationCode(String phone) {
+        checkPhoneNumberDuplicate(phone);
+
+        String code = GeneratorRandomUtil.generateRandomNum();
+        LocalDateTime now = LocalDateTime.now();
+
+        redisTemplate.opsForValue().set(PREFIX_VERIFICATION_CODE + phone, code, Duration.ofMinutes(VERIFICATION_TIME));
+        redisTemplate.opsForValue().set(PREFIX_VERIFICATION_TIME + phone, now.toString(), Duration.ofMinutes(VERIFICATION_TIME));
+
+        String emailAddress = emailService.getServerEmail();
+        return new PhoneVerificationResultResponse(code, emailAddress);
+    }
+
+    @Transactional
+    public void verifyPhoneCode(String phone) {
+        String code = redisTemplate.opsForValue().get(PREFIX_VERIFICATION_CODE + phone);
+        String time = redisTemplate.opsForValue().get(PREFIX_VERIFICATION_TIME + phone);
+
+        if (code == null || time == null) {
+            throw new CustomException(AuthErrorCode.INVALID_VERIFICATION_CODE);
+        }
+
+        LocalDateTime createdAt = LocalDateTime.parse(time);
+        boolean result = emailService.extractCodeByPhoneNumber(code, phone, createdAt);
+
+        if (!result) {
+            throw new CustomException(AuthErrorCode.INVALID_VERIFICATION_CODE);
+        }
+
+        redisTemplate.delete(PREFIX_VERIFICATION_CODE + phone);
+        redisTemplate.delete(PREFIX_VERIFICATION_TIME + phone);
+
+        redisTemplate.opsForValue().set(phone, VERIFIED, Duration.ofMinutes(VERIFICATION_TIME));
     }
 }
