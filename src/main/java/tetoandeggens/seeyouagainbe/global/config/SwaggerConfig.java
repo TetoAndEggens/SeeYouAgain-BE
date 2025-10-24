f6a66b9f991c73325fdac026e41c402e600214cc
JWMin556~ <min981626@gmail.com>
2025-10-24 22:41:43 +0900

    fix: #1 modify a mixed content error in swagger by setting the server url to a relative path ("/")

--- src/main/java/tetoandeggens/seeyouagainbe/global/config/SwaggerConfig.java
+++ src/main/java/tetoandeggens/seeyouagainbe/global/config/SwaggerConfig.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.List;
 
 @Configuration
 @Profile({"local", "dev", "prod"})
@@ -15,6 +18,11 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server()
+                                .url("/")
+                                .description("현재 서버")
+                ))
                 .info(new Info()
                         .title("SeeYouAgain API Document")
                         .description("SeeYouAgain 서버 API 명세서입니다.")
