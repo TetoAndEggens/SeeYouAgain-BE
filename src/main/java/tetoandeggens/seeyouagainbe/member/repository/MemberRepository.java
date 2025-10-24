b7d0b04796c09a0fe903f28a6103dc102c58359f
JWMin556~ <min981626@gmail.com>
2025-10-24 16:09:07 +0900

    refactor: #1 delete unused method in MemberRepository

--- src/main/java/tetoandeggens/seeyouagainbe/member/repository/MemberRepository.java
+++ src/main/java/tetoandeggens/seeyouagainbe/member/repository/MemberRepository.java
@@ -1,15 +1,13 @@
-package tetoandeggens.seeyouagainbe.domain.member.repository;
+package tetoandeggens.seeyouagainbe.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import tetoandeggens.seeyouagainbe.domain.member.entity.Member;
+import tetoandeggens.seeyouagainbe.member.entity.Member;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByLoginIdAndIsDeletedFalse(String loginId);
-
-    Optional<Member> findByPhoneNumberAndIsDeletedFalse(String phoneNumber);
 
     Optional<Member> findByIdAndIsDeletedFalse(Long id);
 
