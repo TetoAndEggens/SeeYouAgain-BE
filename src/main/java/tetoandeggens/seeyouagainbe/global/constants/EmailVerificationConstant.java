83ad8cb928d03556b26976b0d6f61c23e9b44cf6
JWMin556~ <min981626@gmail.com>
2025-10-27 03:19:19 +0900

    feat: #1 add email verification constants and logic

--- src/main/java/tetoandeggens/seeyouagainbe/global/constants/EmailVerificationConstant.java
+++ src/main/java/tetoandeggens/seeyouagainbe/global/constants/EmailVerificationConstant.java
@@ -4,9 +4,16 @@ public class EmailVerificationConstant {
 
     public static final int VERIFICATION_TIME = 5; // (분)
     public static final String VERIFICATION_CODE_PATTERN = "\\b(\\d{6})\\b";
-    public static final String PREFIX_VERIFICATION_CODE = "phone:code:";
-    public static final String PREFIX_VERIFICATION_TIME = "phone:time:";
-    public static final String VERIFIED = "verified";
+    public static final String PREFIX_VERIFICATION_CODE = "verify:phone:code:";
+    public static final String PREFIX_VERIFICATION_TIME = "verify:phone:time:";
+    public static final String VERIFIED = "verify:phone:verified:";
+
+    public static final String PREFIX_SOCIAL_VERIFICATION_CODE = "social:phone:code:";
+    public static final String PREFIX_SOCIAL_VERIFICATION_TIME = "social:phone:time:";
+    public static final String PREFIX_SOCIAL_VERIFIED = "social:phone:verified:";
+    public static final String PREFIX_SOCIAL_PROVIDER = "social:phone:provider:";
+    public static final String PREFIX_SOCIAL_ID = "social:phone:socialid:";
+    public static final String PREFIX_SOCIAL_PROFILE = "social:phone:profile:";
 
     public static final String IMAP_PROTOCOL = "imaps";
     public static final String INBOX = "INBOX";
