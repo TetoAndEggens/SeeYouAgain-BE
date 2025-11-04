29e6fc8cff13a8c9c292cde0ea09bdbc3588ca43
bronze_yun_4521 <ehddbs452100@naver.com>
2025-11-04 22:59:16 +0900

    refactor: #15 update field

--- src/main/java/tetoandeggens/seeyouagainbe/animal/entity/AbandonedAnimalS3Profile.java
+++ src/main/java/tetoandeggens/seeyouagainbe/animal/entity/AbandonedAnimalS3Profile.java
@@ -1,9 +1,9 @@
 package tetoandeggens.seeyouagainbe.animal.entity;
 
-import java.time.LocalDate;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,24 +14,26 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import tetoandeggens.seeyouagainbe.common.enums.ImageType;
 import tetoandeggens.seeyouagainbe.global.entity.BaseEntity;
 
 @Entity
-@Table(name = "ABANDONED_ANIMAL_PROFILE")
+@Table(name = "ABANDONED_ANIMAL_S3_PROFILE")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class AbandonedAnimalProfile extends BaseEntity {
+public class AbandonedAnimalS3Profile extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "abandoned_animal_profile_id")
+	@Column(name = "abandoned_animal_s3_profile_id")
 	private Long id;
 
-	@Column(name = "profile")
-	private String profile;
+	@Column(name = "object_key", unique = true)
+	private String objectKey;
 
-	@Column(name = "happen_date")
-	private LocalDate happenDate;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "image_type")
+	private ImageType imageType = ImageType.WEBP;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "abandoned_animal_id")
