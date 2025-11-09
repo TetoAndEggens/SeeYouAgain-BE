5fb6bd0bd24a1395c20e80c108d67c3c49a90a60
JWMin556~ <min981626@gmail.com>
2025-11-10 01:45:09 +0900

    refactor: #1 manage duplicated resolve methods into single resolveCookieValue

--- src/main/java/tetoandeggens/seeyouagainbe/global/constants/AuthConstants.java
+++ src/main/java/tetoandeggens/seeyouagainbe/global/constants/AuthConstants.java
@@ -2,6 +2,7 @@ package tetoandeggens.seeyouagainbe.global.constants;
 
 public class AuthConstants {
     public static final String ROLE_CLAIM = "role";
+    public static final String SOCIAL_TEMP_TOKEN = "socialTempToken";
     public static final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
     public static final String ACCESS_TOKEN_COOKIE_NAME = "accessToken";
 
