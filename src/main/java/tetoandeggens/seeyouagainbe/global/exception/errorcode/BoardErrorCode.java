3308f0f012a6800beb2b0e18244fd7721fadd9f6
ehddbs4521 <ehddbs452100@naver.com>
2025-11-19 02:07:00 +0900

    feat: #29 add custom error code

--- src/main/java/tetoandeggens/seeyouagainbe/global/exception/errorcode/BoardErrorCode.java
+++ src/main/java/tetoandeggens/seeyouagainbe/global/exception/errorcode/BoardErrorCode.java
@@ -7,10 +7,10 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum AnimalErrorCode implements ErrorCode {
+public enum BoardErrorCode implements ErrorCode {
 
-	ANIMAL_NOT_FOUND("ANIMAL_001", "유기 동물을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-	INVALID_ANIMAL_TYPE_CODE("ANIMAL_002", "유효하지 않은 동물 타입 코드입니다.", HttpStatus.BAD_REQUEST);
+	BOARD_NOT_FOUND("BOARD_001", "게시물을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+	INVALID_CONTENT_TYPE("BOARD_002", "유효하지 않은 게시물 타입입니다.", HttpStatus.BAD_REQUEST);
 
 	private final String code;
 	private final String message;
