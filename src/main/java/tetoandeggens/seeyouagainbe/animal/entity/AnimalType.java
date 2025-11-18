87aa49ca158cd3308cce28b086e027ee51bb03fc
ehddbs4521 <ehddbs452100@naver.com>
2025-11-19 02:06:34 +0900

    feat: #29 get enum field from using string field

--- src/main/java/tetoandeggens/seeyouagainbe/animal/entity/AnimalType.java
+++ src/main/java/tetoandeggens/seeyouagainbe/animal/entity/AnimalType.java
@@ -1,15 +1,27 @@
 package tetoandeggens.seeyouagainbe.animal.entity;
 
+import java.util.Arrays;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import tetoandeggens.seeyouagainbe.global.exception.CustomException;
+import tetoandeggens.seeyouagainbe.global.exception.errorcode.AnimalErrorCode;
 
 @Getter
 @RequiredArgsConstructor
 public enum AnimalType {
 
-	ABANDONED("유기"),
-	MISSING("실종"),
-	WITNESS("목격");
+	ABANDONED("ABANDONED", "유기"),
+	MISSING("MISSING", "실종"),
+	WITNESS("WITNESS", "목격");
 
+	private final String code;
 	private final String type;
+
+	public static AnimalType fromCode(String code) {
+		return Arrays.stream(AnimalType.values())
+			.filter(animalType -> animalType.code.equals(code))
+			.findFirst()
+			.orElseThrow(() -> new CustomException(AnimalErrorCode.INVALID_ANIMAL_TYPE_CODE));
+	}
 }
