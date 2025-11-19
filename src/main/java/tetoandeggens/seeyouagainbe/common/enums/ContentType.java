be5e5754aecb9c78c094075eb72999a4c289969f
ehddbs4521 <ehddbs452100@naver.com>
2025-11-19 22:40:34 +0900

    feat: #29 extract enum class with string class

--- src/main/java/tetoandeggens/seeyouagainbe/common/enums/ContentType.java
+++ src/main/java/tetoandeggens/seeyouagainbe/common/enums/ContentType.java
@@ -1,7 +1,11 @@
 package tetoandeggens.seeyouagainbe.common.enums;
 
+import java.util.Arrays;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import tetoandeggens.seeyouagainbe.global.exception.CustomException;
+import tetoandeggens.seeyouagainbe.global.exception.errorcode.BoardErrorCode;
 
 @Getter
 @RequiredArgsConstructor
@@ -10,4 +14,11 @@ public enum ContentType {
 	MISSING("실종");
 
 	private final String type;
+
+	public static ContentType fromCode(String code) {
+		return Arrays.stream(ContentType.values())
+			.filter(contentType -> contentType.name().equalsIgnoreCase(code))
+			.findFirst()
+			.orElseThrow(() -> new CustomException(BoardErrorCode.INVALID_CONTENT_TYPE));
+	}
 }
