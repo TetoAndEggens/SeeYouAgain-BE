14886ec7e93a21098d4515487442717091b745f1
JWMin556~ <min981626@gmail.com>
2025-11-23 17:03:43 +0900

    feat: #24 remove unnecessary codes about AdminViolation

--- src/main/java/tetoandeggens/seeyouagainbe/global/exception/errorcode/AdminErrorCode.java
+++ src/main/java/tetoandeggens/seeyouagainbe/global/exception/errorcode/AdminErrorCode.java
@@ -9,8 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum AdminErrorCode implements ErrorCode {
 
     VIOLATION_NOT_FOUND("ADMIN_001", "신고 내역을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    ALREADY_PROCESSED_VIOLATION("ADMIN_002", "이미 처리된 신고입니다.", HttpStatus.BAD_REQUEST),
-    ADMIN_PERMISSION_DENIED("ADMIN_003", "관리자 권한이 필요합니다.", HttpStatus.FORBIDDEN);
+    ADMIN_PERMISSION_DENIED("ADMIN_002", "관리자 권한이 필요합니다.", HttpStatus.FORBIDDEN);
 
     private final String code;
     private final String message;
