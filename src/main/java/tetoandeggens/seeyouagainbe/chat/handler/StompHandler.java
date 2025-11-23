2e484fb5f9f384cc5702b420f4a21997f223a25a
ehddbs4521 <ehddbs452100@naver.com>
2025-11-23 19:59:23 +0900

    refactor: #35 validate jwt token when sending message

--- src/main/java/tetoandeggens/seeyouagainbe/chat/handler/StompHandler.java
+++ src/main/java/tetoandeggens/seeyouagainbe/chat/handler/StompHandler.java
@@ -1,11 +1,12 @@
 package tetoandeggens.seeyouagainbe.chat.handler;
 
+import static tetoandeggens.seeyouagainbe.global.constants.AuthCommonConstants.*;
+
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -17,37 +18,33 @@ import tetoandeggens.seeyouagainbe.global.exception.errorcode.AuthErrorCode;
 @RequiredArgsConstructor
 public class StompHandler implements ChannelInterceptor {
 
-	private static final String AUTHORIZATION_HEADER = "Authorization";
-	private static final String BEARER_PREFIX = "Bearer ";
-
 	private final TokenProvider tokenProvider;
 
 	@Override
 	public Message<?> preSend(Message<?> message, MessageChannel channel) {
 		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+		StompCommand command = accessor.getCommand();
 
-		if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-			String token = extractToken(accessor);
+		if (StompCommand.CONNECT.equals(command)) {
+			Long memberId = (Long)accessor.getSessionAttributes().get("memberId");
 
-			if (token == null) {
+			if (memberId == null) {
+				throw new CustomException(AuthErrorCode.INVALID_TOKEN);
+			}
+		} else if (StompCommand.SEND.equals(command) || StompCommand.SUBSCRIBE.equals(command)) {
+			String accessToken = (String)accessor.getSessionAttributes().get(ACCESS_TOKEN_COOKIE_NAME);
+
+			if (accessToken == null) {
 				throw new CustomException(AuthErrorCode.INVALID_TOKEN);
 			}
 
-			tokenProvider.validateToken(token);
-			Authentication authentication = tokenProvider.getAuthenticationByAccessToken(token);
-			accessor.setUser(authentication);
+			try {
+				tokenProvider.validateToken(accessToken);
+			} catch (Exception e) {
+				throw new CustomException(AuthErrorCode.INVALID_TOKEN);
+			}
 		}
 
 		return message;
-	}
-
-	private String extractToken(StompHeaderAccessor accessor) {
-		String authHeader = accessor.getFirstNativeHeader(AUTHORIZATION_HEADER);
-
-		if (authHeader != null && authHeader.startsWith(BEARER_PREFIX)) {
-			return authHeader.substring(BEARER_PREFIX.length());
-		}
-
-		return null;
 	}
 }
