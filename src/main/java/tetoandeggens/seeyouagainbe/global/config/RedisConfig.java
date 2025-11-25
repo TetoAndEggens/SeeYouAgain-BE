9052353b7f1394829eaddda4d11969e530d7c733
ehddbs4521 <ehddbs452100@naver.com>
2025-11-25 20:34:34 +0900

    feat: #35 add redis test container

--- src/main/java/tetoandeggens/seeyouagainbe/global/config/RedisConfig.java
+++ src/main/java/tetoandeggens/seeyouagainbe/global/config/RedisConfig.java
@@ -3,6 +3,7 @@ package tetoandeggens.seeyouagainbe.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -14,6 +15,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import tetoandeggens.seeyouagainbe.chat.sub.RedisSubscriber;
 
+@Profile("!test")
 @Configuration
 public class RedisConfig {
 
--- src/test/java/tetoandeggens/seeyouagainbe/global/ServiceTest.java
+++ src/test/java/tetoandeggens/seeyouagainbe/global/ServiceTest.java
@@ -1,11 +1,20 @@
 package tetoandeggens.seeyouagainbe.global;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @ActiveProfiles("test")
 @SpringBootTest
 @Transactional
 public abstract class ServiceTest {
+
+	@MockitoBean
+	protected RedisTemplate<String, String> redisTemplate;
+
+	@MockitoBean
+	protected ChannelTopic channelTopic;
 }
