5fe5410c6c91952ba74d82a204fa5b6f046e1bd2
JWMin556~ <min981626@gmail.com>
2025-12-01 16:51:15 +0900

    feat: #40 add FCM token and notification keyword repositories

---
+++ src/main/java/tetoandeggens/seeyouagainbe/notification/repository/NotificationKeywordRepository.java
@@ -0,0 +1,19 @@
+package tetoandeggens.seeyouagainbe.notification.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tetoandeggens.seeyouagainbe.notification.entity.KeywordCategoryType;
+import tetoandeggens.seeyouagainbe.notification.entity.KeywordType;
+import tetoandeggens.seeyouagainbe.notification.entity.NotificationKeyword;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface NotificationKeywordRepository extends JpaRepository<NotificationKeyword, Long> {
+
+    List<NotificationKeyword> findAllByMemberId(Long memberId);
+
+    boolean existsByMemberIdAndKeywordAndKeywordTypeAndKeywordCategoryType(
+            Long memberId, String keyword, KeywordType keywordType, KeywordCategoryType keywordCategoryType);
+
+    Optional<NotificationKeyword> findByIdAndMemberId(Long id, Long memberId);
+}
