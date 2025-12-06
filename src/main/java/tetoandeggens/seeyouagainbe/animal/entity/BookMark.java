b88ac7419e47a1154febdd6178b8c3e7d3741784
JWMin556~ <min981626@gmail.com>
2025-12-07 03:06:04 +0900

    refactor: #49 add builder constructor to BookMark entity

--- src/main/java/tetoandeggens/seeyouagainbe/animal/entity/BookMark.java
+++ src/main/java/tetoandeggens/seeyouagainbe/animal/entity/BookMark.java
@@ -10,6 +10,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import tetoandeggens.seeyouagainbe.global.entity.BaseEntity;
@@ -26,14 +27,25 @@ public class BookMark extends BaseEntity {
 	@Column(name = "book_mark_id")
 	private Long id;
 
-	@Column(name = "is_deleted")
+	@Column(name = "is_deleted", nullable = false)
 	private Boolean isDeleted;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_id")
+	@JoinColumn(name = "member_id", nullable = false)
 	private Member member;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "animal_id")
+	@JoinColumn(name = "animal_id", nullable = false)
 	private Animal animal;
+
+    @Builder
+    public BookMark(Member member, Animal animal) {
+        this.member = member;
+        this.animal = animal;
+        this.isDeleted = false;
+    }
+
+    public void toggleDelete() {
+        this.isDeleted = !this.isDeleted;
+    }
 }
