c1c83c175acf48e00f0cf0e72e0b4cafd6e32ae7
JWMin556~ <min981626@gmail.com>
2025-12-07 03:06:21 +0900

    feat: #49 add DTOs for bookmark API responses

---
+++ src/main/java/tetoandeggens/seeyouagainbe/animal/dto/response/BookMarkAnimalResponse.java
@@ -0,0 +1,35 @@
+package tetoandeggens.seeyouagainbe.animal.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import tetoandeggens.seeyouagainbe.animal.entity.BookMark;
+import tetoandeggens.seeyouagainbe.animal.entity.Species;
+
+@Builder
+@Schema(name = "BookMarkAnimalResponse: 북마크한 동물 정보 응답 Dto")
+public record BookMarkAnimalResponse(
+        @Schema(description = "북마크 고유 ID", example = "1")
+        Long bookMarkId,
+
+        @Schema(description = "동물 고유 ID", example = "123")
+        Long animalId,
+
+        @Schema(description = "동물 종", example = "DOG")
+        Species species,
+
+        @Schema(description = "품종", example = "치와와")
+        String breedType,
+
+        @Schema(description = "보호 상태", example = "보호중")
+        String processState
+) {
+    public static BookMarkAnimalResponse from(BookMark bookMark) {
+        return BookMarkAnimalResponse.builder()
+                .bookMarkId(bookMark.getId())
+                .animalId(bookMark.getAnimal().getId())
+                .species(bookMark.getAnimal().getSpecies())
+                .breedType(bookMark.getAnimal().getBreedType().getName())
+                .processState(bookMark.getAnimal().getProcessState())
+                .build();
+    }
+}
