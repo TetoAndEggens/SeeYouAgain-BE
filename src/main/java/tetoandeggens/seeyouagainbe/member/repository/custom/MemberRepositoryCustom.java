5b363a761834b65a093619ad84fc54dff61fba8d
JWMin556~ <min981626@gmail.com>
2025-12-06 14:10:30 +0900

    refactor: #45 implement findDeletedMemberForRestore to validate loginId/phoneNumber/isDeleted in single query

--- src/main/java/tetoandeggens/seeyouagainbe/member/repository/custom/MemberRepositoryCustom.java
+++ src/main/java/tetoandeggens/seeyouagainbe/member/repository/custom/MemberRepositoryCustom.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 public interface MemberRepositoryCustom {
 
-    Optional<Member> findByLoginIdIncludingDeleted(String loginId);
-
     Optional<Member> findByPhoneNumberIncludingDeleted(String phoneNumber);
+
+    Optional<Member> findDeletedMemberForRestore(String loginId, String phoneNumber);
 }
