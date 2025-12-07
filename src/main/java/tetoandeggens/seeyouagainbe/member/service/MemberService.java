a12bc58f21e1e146dee68c5eb96d94cc6e75a7cf
JWMin556~ <min981626@gmail.com>
2025-12-07 18:53:18 +0900

    refactor: #49 update MemberService to use void return type for push toggle

--- src/main/java/tetoandeggens/seeyouagainbe/member/service/MemberService.java
+++ src/main/java/tetoandeggens/seeyouagainbe/member/service/MemberService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tetoandeggens.seeyouagainbe.member.dto.request.UpdatePushEnabledRequest;
-import tetoandeggens.seeyouagainbe.member.dto.response.UpdatePushEnabledResponse;
 import tetoandeggens.seeyouagainbe.member.entity.Member;
 import tetoandeggens.seeyouagainbe.member.repository.MemberRepository;
 import tetoandeggens.seeyouagainbe.global.exception.CustomException;
@@ -23,12 +22,10 @@ public class MemberService {
     }
 
     @Transactional
-    public UpdatePushEnabledResponse updatePushEnabled(Long memberId, UpdatePushEnabledRequest request) {
+    public void updatePushEnabled(Long memberId, UpdatePushEnabledRequest request) {
         Member member = memberRepository.findByIdAndIsDeletedFalse(memberId)
                 .orElseThrow(() -> new CustomException(AuthErrorCode.MEMBER_NOT_FOUND));
 
         member.updatePushEnabled(request.isPushEnabled());
-
-        return UpdatePushEnabledResponse.from(member.getIsPushEnabled());
     }
 }
