ab4363133dd09dd393624fbed683612d688a39bc
JWMin556~ <min981626@gmail.com>
2026-01-24 23:03:02 +0900

    feat: #88 add getMyInfo method to MemberService

--- src/main/java/tetoandeggens/seeyouagainbe/member/service/MemberService.java
+++ src/main/java/tetoandeggens/seeyouagainbe/member/service/MemberService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tetoandeggens.seeyouagainbe.member.dto.request.UpdatePushEnabledRequest;
+import tetoandeggens.seeyouagainbe.member.dto.response.MyInfoResponse;
 import tetoandeggens.seeyouagainbe.member.entity.Member;
 import tetoandeggens.seeyouagainbe.member.repository.MemberRepository;
 import tetoandeggens.seeyouagainbe.global.exception.CustomException;
@@ -16,9 +17,11 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     @Transactional(readOnly = true)
-    public Member findById(Long memberId) {
-        return memberRepository.findByIdAndIsDeletedFalse(memberId)
+    public MyInfoResponse getMyInfo(Long memberId) {
+        Member member = memberRepository.findByIdAndIsDeletedFalse( memberId )
                 .orElseThrow(() -> new CustomException(AuthErrorCode.MEMBER_NOT_FOUND));
+
+        return MyInfoResponse.from(member.getNickName(), member.getProfile());
     }
 
     @Transactional
