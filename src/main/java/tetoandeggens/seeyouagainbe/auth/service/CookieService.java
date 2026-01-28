2d954595a2e3f5a0a0c77497178233e2ada8ea23
JWMin556~ <min981626@gmail.com>
2026-01-28 23:29:42 +0900

    refactor: #94 inject CookieUtil as dependency in CookieService

--- src/main/java/tetoandeggens/seeyouagainbe/auth/service/CookieService.java
+++ src/main/java/tetoandeggens/seeyouagainbe/auth/service/CookieService.java
@@ -12,40 +12,42 @@ import static tetoandeggens.seeyouagainbe.global.constants.AuthCommonConstants.*
 @RequiredArgsConstructor
 public class CookieService {
 
+    private final CookieUtil cookieUtil;
+
     public void setAccessTokenCookie(HttpServletResponse response, String accessToken, long expirationSec) {
-        CookieUtil.setCookie(response, ACCESS_TOKEN_COOKIE_NAME, accessToken, expirationSec);
+        cookieUtil.setCookie(response, ACCESS_TOKEN_COOKIE_NAME, accessToken, expirationSec);
     }
 
     public void setRefreshTokenCookie(HttpServletResponse response, String refreshToken, long expirationSec) {
-        CookieUtil.setCookie(response, REFRESH_TOKEN_COOKIE_NAME, refreshToken, expirationSec);
+        cookieUtil.setCookie(response, REFRESH_TOKEN_COOKIE_NAME, refreshToken, expirationSec);
     }
 
     public void setSocialTempTokenCookie(HttpServletResponse response, String tempToken, long expirationSec) {
-        CookieUtil.setCookie(response, SOCIAL_TEMP_TOKEN, tempToken, expirationSec);
+        cookieUtil.setCookie(response, SOCIAL_TEMP_TOKEN, tempToken, expirationSec);
     }
 
     public String resolveAccessToken(HttpServletRequest request) {
-        return CookieUtil.resolveCookieValue(request, ACCESS_TOKEN_COOKIE_NAME);
+        return cookieUtil.resolveCookieValue(request, ACCESS_TOKEN_COOKIE_NAME);
     }
 
     public String resolveRefreshToken(HttpServletRequest request) {
-        return CookieUtil.resolveCookieValue(request, REFRESH_TOKEN_COOKIE_NAME);
+        return cookieUtil.resolveCookieValue(request, REFRESH_TOKEN_COOKIE_NAME);
     }
 
     public String resolveSocialTempToken(HttpServletRequest request) {
-        return CookieUtil.resolveCookieValue(request, SOCIAL_TEMP_TOKEN);
+        return cookieUtil.resolveCookieValue(request, SOCIAL_TEMP_TOKEN);
     }
 
     public void deleteTempTokenCookie(HttpServletResponse response) {
-        CookieUtil.deleteCookie(response, SOCIAL_TEMP_TOKEN);
+        cookieUtil.deleteCookie(response, SOCIAL_TEMP_TOKEN);
     }
 
     public void deleteAccessTokenCookie(HttpServletResponse response) {
-        CookieUtil.deleteCookie(response, ACCESS_TOKEN_COOKIE_NAME);
+        cookieUtil.deleteCookie(response, ACCESS_TOKEN_COOKIE_NAME);
     }
 
     public void deleteRefreshTokenCookie(HttpServletResponse response) {
-        CookieUtil.deleteCookie(response, REFRESH_TOKEN_COOKIE_NAME);
+        cookieUtil.deleteCookie(response, REFRESH_TOKEN_COOKIE_NAME);
     }
 
     public void deleteAllAuthCookies(HttpServletResponse response) {
