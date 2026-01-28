992aa6c6c960ebe3eaca6fa9564e371a8307fc6c
JWMin556~ <min981626@gmail.com>
2026-01-28 23:30:30 +0900

    refactor: #94 inject CookieUtil in OAuth2AuthorizationRequestRepository

--- src/main/java/tetoandeggens/seeyouagainbe/auth/oauth2/repository/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ src/main/java/tetoandeggens/seeyouagainbe/auth/oauth2/repository/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -2,6 +2,7 @@ package tetoandeggens.seeyouagainbe.auth.oauth2.repository;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.stereotype.Component;
@@ -14,12 +15,15 @@ import java.util.Base64;
 import static tetoandeggens.seeyouagainbe.global.constants.AuthCommonConstants.*;
 
 @Component
+@RequiredArgsConstructor
 public class HttpCookieOAuth2AuthorizationRequestRepository
         implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
 
+    private final CookieUtil cookieUtil;
+
     @Override
     public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
-        String serialized = CookieUtil.resolveCookieValue(request, OAUTH2_AUTH_REQUEST_COOKIE_NAME);
+        String serialized = cookieUtil.resolveCookieValue(request, OAUTH2_AUTH_REQUEST_COOKIE_NAME);
         if (!StringUtils.hasText(serialized)) return null;
         byte[] bytes = Base64.getUrlDecoder().decode(serialized);
         return (OAuth2AuthorizationRequest) SerializationUtils.deserialize(bytes);
@@ -36,11 +40,11 @@ public class HttpCookieOAuth2AuthorizationRequestRepository
 
         byte[] bytes = SerializationUtils.serialize(authorizationRequest);
         String serialized = Base64.getUrlEncoder().encodeToString(bytes);
-        CookieUtil.setCookie(response, OAUTH2_AUTH_REQUEST_COOKIE_NAME, serialized, COOKIE_EXPIRE_SECONDS);
+        cookieUtil.setCookie(response, OAUTH2_AUTH_REQUEST_COOKIE_NAME, serialized, COOKIE_EXPIRE_SECONDS);
 
         String redirectUriAfterLogin = request.getParameter(REDIRECT_URI_PARAM_COOKIE_NAME);
         if (StringUtils.hasText(redirectUriAfterLogin)) {
-            CookieUtil.setCookie(response, REDIRECT_URI_PARAM_COOKIE_NAME, redirectUriAfterLogin, COOKIE_EXPIRE_SECONDS);
+            cookieUtil.setCookie(response, REDIRECT_URI_PARAM_COOKIE_NAME, redirectUriAfterLogin, COOKIE_EXPIRE_SECONDS);
         }
     }
 
@@ -53,7 +57,7 @@ public class HttpCookieOAuth2AuthorizationRequestRepository
     }
 
     public void removeAuthorizationRequestCookies(HttpServletRequest request, HttpServletResponse response) {
-        CookieUtil.deleteCookie(response, OAUTH2_AUTH_REQUEST_COOKIE_NAME);
-        CookieUtil.deleteCookie(response, REDIRECT_URI_PARAM_COOKIE_NAME);
+        cookieUtil.deleteCookie(response, OAUTH2_AUTH_REQUEST_COOKIE_NAME);
+        cookieUtil.deleteCookie(response, REDIRECT_URI_PARAM_COOKIE_NAME);
     }
 }
